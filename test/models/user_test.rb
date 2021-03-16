9e240e1d43705337415ba66bad69141c82b19b54
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-17 00:44:32 +0530

    Re-write test cases in snake case naming convention

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,49 +6,49 @@ class UserTest < ActiveSupport::TestCase
                       password: "123456", password_confirmation: "123456")
   end
 
-  test "user_should_be_valid" do 
+  def test_user_should_be_valid  
     assert @user.valid?
   end
 
-  test "first_name_should_not_be_blank" do
+  def test_first_name_should_not_be_blank
     @user.first_name = ""
     assert_not @user.valid?
   end
 
-  test "last_name_should_not_be_blank" do
+  def test_last_name_should_not_be_blank
     @user.last_name = ""
     assert_not @user.valid?
   end
 
-  test "email_should_not_be_blank" do
+  def test_email_should_not_be_blank
     @user.email = ""
     assert_not @user.valid?
   end
 
-  test "first_name_should_not_exceed_50_characters" do
+  def test_first_name_should_not_exceed_50_characters
     @user.first_name = "a" * 51
     assert_not @user.valid?
   end
 
-  test "last_name_should_not_exceed_50_characters" do
+  def test_last_name_should_not_exceed_50_characters
     @user.last_name = "a" * 51
     assert_not @user.valid?
   end
 
-  test "email_should_be_unique" do
+  def test_email_should_be_unique
     duplicate_user = @user.dup 
     @user.save
     assert_not duplicate_user.valid?
   end
 
-  test "email_addresses_should_be_saved_as_lower-case" do 
+  def test_email_addresses_should_be_saved_as_lower_case 
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email 
   end
 
-  test "email_validation_should_accept_valid_addresses" do
+  def test_email_validation_should_accept_valid_addresses
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
                         first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -57,7 +57,7 @@ class UserTest < ActiveSupport::TestCase
     end 
   end
 
-  test "email validation should reject invalid addresses" do
+  def test_email_validation_should_reject_invalid_addresses
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. 
                           foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -66,21 +66,21 @@ class UserTest < ActiveSupport::TestCase
     end 
   end
 
-  test "user_should_have_a_valid_role" do
+  def test_user_should_have_a_valid_role
     assert @user.valid? if @user.role == "standard" || "administrator"
   end
 
-  test "password_should_not_be_blank" do
+  def test_password_should_not_be_blank
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
-  test "password_should_have_a_minimum_length" do
+  def test_password_should_have_a_minimum_length
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 
-  test "password_and_password_confirmation_should_match" do
+  def test_password_and_password_confirmation_should_match
     assert @user.valid? if @user.password == @user.password_confirmation
   end
 
