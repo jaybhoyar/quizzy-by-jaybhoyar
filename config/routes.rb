46a4c431d13568e96d2864a7c88056989a047136
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-18 23:56:57 +0530

    Generate quizzes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resource :sessions, only: %i[create destroy]
+  resources :quizzes, only: %i[create index show]
+
   root "home#index"
   get '*path', to: 'home#index', via: :all
 end
