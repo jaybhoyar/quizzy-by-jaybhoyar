5a89dea40eca6084d6ffa4f5fea230ee2bffcd3e
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-18 14:39:49 +0530

    Add quiz model with user reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   enum role: [:standard, :administrator]
 
+  has_many :quizzes
   validates :email, presence: true,
                      format: { with: VALID_EMAIL_REGEX },
                      uniqueness: { case_sensitive: false }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_051135) do
+ActiveRecord::Schema.define(version: 2021_03_18_090309) do
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_03_18_051135) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "quizzes", "users"
 end
