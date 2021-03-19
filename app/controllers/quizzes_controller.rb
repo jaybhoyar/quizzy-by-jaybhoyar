1f99c27497929e8bf5adce2f95d1a39e1efbb436
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-20 01:14:35 +0530

    Add update action and update api in quiz controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,5 @@
 class QuizzesController < ApplicationController
+  before_action :load_quiz, only: %i[update]
   
   def index
     quizzes = Quiz.all.order("created_at DESC")
@@ -9,15 +10,31 @@ class QuizzesController < ApplicationController
     quiz = Quiz.new(quiz_params)
     quiz.user_id = @current_user.id
     if quiz.save
-      render status: :ok, json: {notice: "Quiz created successfully!"}
+      render status: :ok, json: { notice: "Quiz created successfully!" }
     else
-      render status: :unprocessable_entity, json: {errors: quiz.errors.full_messages.to_sentence}
+      render status: :unprocessable_entity, json: { errors: quiz.errors.full_messages.to_sentence }
     end
   end
+
+  def update
+    if @quiz.blank?
+      render status: :not_found, json: { notice: "Quiz not found" }
+    elsif @quiz.update(quiz_params)
+      render status: :ok, json: { notice: "Quiz updated successfully!" }
+    else
+      render status: :unprocessable_entity, json: { errors: @quiz.errors.full_messages.to_sentence }
+    end
+  end
+
+
 
   private
     def quiz_params
       params.require(:quiz).permit(:name)
+    end
+
+    def load_quiz
+      @quiz = Quiz.find(params[:id])
     end
 
 
--- app/javascript/src/apis/quiz.js
+++ app/javascript/src/apis/quiz.js
@@ -4,9 +4,12 @@ const list = () => axios.get("/quizzes");
 
 const create = (payload) => axios.post("/quizzes", payload);
 
+const update = ({ id, payload }) => axios.put(`/quizzes/${id}`, payload);
+
 const quizzesApi = {
 	list,
 	create,
+	update,
 };
 
 export default quizzesApi;
