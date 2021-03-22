e15ab3b88aa33aca10340758546c87d33f023255
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-22 10:06:33 +0530

    Add quiz details in show quiz component

--- app/javascript/src/components/Quiz/ShowQuiz.jsx
+++ app/javascript/src/components/Quiz/ShowQuiz.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+import quizzesApi from "apis/quiz";
 
 const ShowQuiz = () => {
-  
+	const { id } = useParams();
+	const [quizDetails, setQuizDetails] = useState("");
+
+	const fetchQuizDetails = async () => {
+		try {
+			const response = await quizzesApi.show(id);
+			setQuizDetails(response.data.quiz);
+		} catch (error) {
+			//
+		}
+	};
+
+	useEffect(() => {
+		fetchQuizDetails();
+	}, []);
+
 	return (
 		<div className="py-10 w-2/3 mx-auto">
 			<div className="flex items-center justify-between h-16">
 				<div className="flex items-center">
-					<h1 className="text-black font-bold text-2xl"></h1>
+					<h1 className="text-black font-bold text-2xl">
+						{`${quizDetails.name} quiz`}
+					</h1>
 				</div>
 				<div className="flex items-center justify-between">
 					<Link
-						to="/"
-						className="flex justify-center px-6 py-3
-     text-xl font-medium leading-5 text-white transition duration-150
-     ease-in-out bg-quizzy-teal border border-transparent rounded-md"
+						to={`/quizzes/${quizDetails.id}/questions/create`}
+						className="flex justify-center px-6 py-3 text-xl font-medium 
+            leading-5 text-white bg-quizzy-teal border border-transparent rounded-md"
 					>
-						Add new quiz
+						Add questions
 					</Link>
 				</div>
 			</div>
