d9c65b43fe8a9c42f5ab4c4cd82f4276ad0d6e13
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-24 05:20:21 +0530

    Add unit test cases for options in question file

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -4,7 +4,7 @@ class QuestionTest < ActiveSupport::TestCase
   def setup
     user = User.create(first_name: "Sam", last_name: "Smith", email: "sam@example.com", password: "welcome", password_confirmation: "welcome")
     quiz = user.quizzes.new(name:"Animals")
-    @question = quiz.questions.new(title: "What do flamingos eat?")
+    @question = quiz.questions.new(title: "What do flamingos eat?", options_attributes: [value: "correct_option", is_correct:true])
   end
 
   def test_question_should_be_valid
@@ -17,14 +17,26 @@ class QuestionTest < ActiveSupport::TestCase
     assert_equal ["Title can't be blank", "Quiz can't be blank"], @question.errors.full_messages
   end
 
+  def test_question_quiz_should_not_be_blank
+    @question.quiz_id = ""
+    assert_not @question.valid?
+    assert_equal ["Quiz can't be blank"], @question.errors.full_messages
+  end
+
+  def test_question_options_should_not_be_blank
+    @question.options = []
+    assert_not @question.valid?
+    assert_equal ["Options can't be blank", "Quiz can't be blank"], @question.errors.full_messages
+  end
+
   def test_question_title_should_not_exceed_250_characters
     @question.title = "a" * 251
     assert_not @question.valid?
     assert_equal ["Title is too long (maximum is 250 characters)", "Quiz can't be blank"], @question.errors.full_messages
   end
 
-  def test_question_quiz_should_not_be_blank
-    @question.quiz_id = ""
+  def test_question_should_not_exceed_four_options
+    @question.options.length > 4 
     assert_not @question.valid?
     assert_equal ["Quiz can't be blank"], @question.errors.full_messages
   end
