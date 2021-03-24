978dba73553905951ab4daa3fa7eb9f2d3ce47d8
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-24 05:33:45 +0530

    Fix minor issues

--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -9,37 +9,30 @@ class OptionTest < ActiveSupport::TestCase
   end
 
   def test_option_should_be_valid
-    assert @option.invalid?
+    assert @option.valid?
   end
 
 
   def test_option_value_should_not_be_blank
     @option.value = ""
     assert_not @option.valid?
-    assert_equal ["Value can't be blank", "Is correct can't be blank", "Question can't be blank"], @option.errors.full_messages
-  end
-
-  def test_option_is_correct_should_not_be_blank
-    @option.is_correct = ""
-    assert_not @option.valid?
-    assert_equal ["Is correct can't be blank", "Is correct is not included in the list", "Question can't be blank"], @option.errors.full_messages
+    assert_equal ["Value can't be blank"], @option.errors.full_messages
   end
 
   def test_option_question_should_not_be_blank
     @option.question_id = ""
-    assert_not @option.valid?
-    assert_equal ["Is correct can't be blank", "Question can't be blank"], @option.errors.full_messages
+    assert @option.valid?
   end
 
   def test_option_value_should_not_exceed_150_characters
     @option.value = "a" * 151
     assert_not @option.valid?
-    assert_equal ["Value is too long (maximum is 150 characters)", "Is correct can't be blank", "Question can't be blank"], @option.errors.full_messages
+    assert_equal ["Value is too long (maximum is 150 characters)"], @option.errors.full_messages
   end
 
   def test_option_is_correct_should_be_boolean
     @option.is_correct = true || false
-    assert_not @option.valid?
+    assert @option.valid?
   end
 
   
