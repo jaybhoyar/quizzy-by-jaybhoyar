2004aa0149aae0b13c702c60e917017ed93fe476
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-25 14:55:46 +0530

    Add validation for min and max options

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,5 +6,6 @@ class Question < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :options, presence: true
+  validates_length_of :options, maximum: 4, minimum: 2 
   validates :quiz_id, presence: true
 end
