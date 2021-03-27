bd68d68df408294b635178c9df1b0d9cd05eaa9b
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-03-28 03:52:17 +0530

    Add show public quiz component

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -10,6 +10,7 @@ import ShowQuiz from "components/Quiz/ShowQuiz";
 import EditQuiz from "components/Quiz/EditQuiz";
 import CreateQuestion from "components/Question/CreateQuestion";
 import UpdateQuestion from "components/Question/UpdateQuestion";
+import ShowPublicQuiz from "components/PublicQuiz/ShowPublicQuiz";
 
 import { registerIntercepts } from "apis/axios";
 
@@ -21,36 +22,43 @@ const App = ({ currentUser }) => {
 	return (
 		<Router>
 			<ToastContainer />
-			<NavBar currentUser={currentUser} />
 			<div className="bg-quizzy-bg min-h-screen">
 				<Switch>
-					<Route exact path="/" component={Dashboard} />
-					<Route exact path="/login" component={Login} />
 					<Route
 						exact
-						path="/quizzes/:id/show"
-						component={ShowQuiz}
+						path="/public/:slug"
+						component={ShowPublicQuiz}
 					/>
-					<Route
-						exact
-						path="/quizzes/create"
-						component={CreateQuiz}
-					/>
-					<Route
-						exact
-						path="/quizzes/:id/edit"
-						component={EditQuiz}
-					/>
-					<Route
-						exact
-						path="/quizzes/:quiz_id/questions/create"
-						component={CreateQuestion}
-					/>
-					<Route
-						exact
-						path="/quizzes/:quiz_id/questions/:id/edit"
-						component={UpdateQuestion}
-					/>
+					<>
+						<NavBar currentUser={currentUser} />
+						<Route exact path="/" component={Dashboard} />
+						<Route exact path="/login" component={Login} />
+						<Route
+							exact
+							path="/quizzes/:id/show"
+							component={ShowQuiz}
+						/>
+						<Route
+							exact
+							path="/quizzes/create"
+							component={CreateQuiz}
+						/>
+						<Route
+							exact
+							path="/quizzes/:id/edit"
+							component={EditQuiz}
+						/>
+						<Route
+							exact
+							path="/quizzes/:quiz_id/questions/create"
+							component={CreateQuestion}
+						/>
+						<Route
+							exact
+							path="/quizzes/:quiz_id/questions/:id/edit"
+							component={UpdateQuestion}
+						/>
+					</>
 				</Switch>
 			</div>
 		</Router>
---
+++ app/javascript/src/components/PublicQuiz/ShowPublicQuiz.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ShowPublicQuiz() {
+	return (
+		<div className="flex justify-center items-center min-h-screen">
+			<h2 className="text-2xl font-bold">Link is accessible</h2>
+		</div>
+	);
+}
+
+export default ShowPublicQuiz;
